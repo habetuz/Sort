07fff3a84bad00f2fc844d9cb184bce1730fd4c2
Marvin Fuchs <marfinn3@gmail.com>
2021-04-02 21:16:59 +0200

    Add quick sort

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-import {SVG} from './svg.js'
+import { SVG } from './svg.js'
 
 //================================================================================
 // Event listener
 //================================================================================
-document.getElementById("button-random").addEventListener("click", function() {
+document.getElementById("button-random").addEventListener("click", function () {
     runMode = runRandom
     runAgain()
 })
-document.getElementById("button-reverse").addEventListener("click", function() {
+document.getElementById("button-reverse").addEventListener("click", function () {
     runMode = runReverse
     runAgain()
 })
-document.getElementById("button-addone").addEventListener("click", function() {
-    itemCount ++
+document.getElementById("button-addone").addEventListener("click", function () {
+    itemCount++
     runAgain()
 })
-document.getElementById("button-subtractone").addEventListener("click", function() {
+document.getElementById("button-subtractone").addEventListener("click", function () {
     itemCount--
     runAgain()
 })
@@ -35,39 +35,39 @@ var runMode = runRandom
 //================================================================================
 
 var bubbleSort = {
-    svg: SVG().addTo("#bubble-sort").size("100%","100%"),
+    svg: SVG().addTo("#bubble-sort").size("100%", "100%"),
     step: 0,
     items: [],
     idPrefix: "bubble"
 }
 
 function bubbleSortAlgorithm(values) {
-    document.getElementById("bubble-sort").style.height = ((itemCount+1)*40+30) + "px"
+    document.getElementById("bubble-sort").style.height = ((itemCount + 1) * 40 + 30) + "px"
     bubbleSort.svg.clear()
     bubbleSort.step = 0
     bubbleSort.items = []
     publish(copyArray(values), bubbleSort)
-    for(let i = 0; i < values.length; i++) {
-        for(let j = 0; j < values.length-1-i; j++) {
-            if(values[j].value > values[j+1].value) {
-                swap(values, j, j+1)
-                
+    for (let i = 0; i < values.length; i++) {
+        for (let j = 0; j < values.length - 1 - i; j++) {
+            if (values[j].value > values[j + 1].value) {
+                swap(values, j, j + 1)
+
             }
             let copy = copyArray(values)
             copy[j].gotCompared = true
-            copy[j+1].gotCompared = true
+            copy[j + 1].gotCompared = true
             bubbleSortDrawFinishedArea(i)
             publish(copy, bubbleSort)
         }
     }
     bubbleSortDrawFinishedArea(values.length)
-    bubbleSort.svg.size((bubbleSort.items.length-1)*100+60, "100%")
+    bubbleSort.svg.size((bubbleSort.items.length - 1) * 100 + 60, "100%")
 }
 
 function bubbleSortDrawFinishedArea(i) {
-    if(i > 0) bubbleSort.svg.rect(100, (i)*40)
-        .cx((bubbleSort.step-1)*100 + 30)
-        .y(((itemCount+1)*40+30) - ((i+1.5)*40))
+    if (i > 0) bubbleSort.svg.rect(100, (i) * 40)
+        .cx((bubbleSort.step - 1) * 100 + 30)
+        .y(((itemCount + 1) * 40 + 30) - ((i + 1.5) * 40))
         .fill("#546653")
         .back()
 }
@@ -77,41 +77,41 @@ function bubbleSortDrawFinishedArea(i) {
 //================================================================================
 
 var insertSort = {
-    svg: SVG().addTo("#insert-sort").size("100%","100%"),
+    svg: SVG().addTo("#insert-sort").size("100%", "100%"),
     step: 0,
     items: [],
     idPrefix: "insert"
 }
 
 function insertSortAlgorithm(values) {
-    document.getElementById("insert-sort").style.height = ((itemCount+1)*40+30) + "px"
+    document.getElementById("insert-sort").style.height = ((itemCount + 1) * 40 + 30) + "px"
     insertSort.svg.clear()
     insertSort.step = 0
     insertSort.items = []
     publish(copyArray(values), insertSort)
-    for(let i = 1; i < values.length; i++) {
-        for(let j = 0; j < i; j++) {
+    for (let i = 1; i < values.length; i++) {
+        for (let j = 0; j < i; j++) {
             let breakAfter = false
-            if(values[j].value > values[i].value) {
+            if (values[j].value > values[i].value) {
                 shift(values, i, j)
                 breakAfter = true
             }
             let copy = copyArray(values)
             copy[i].gotCompared = true
             copy[j].gotCompared = true
-            console.log("step: " + insertSort.step + " | i: " + i)
+            //console.log("step: " + insertSort.step + " | i: " + i)
             insertSortDrawFinishedArea(i)
             publish(copy, insertSort)
-            if(breakAfter) break
+            if (breakAfter) break
         }
     }
     insertSortDrawFinishedArea(values.length)
-    insertSort.svg.size((insertSort.items.length-1)*100+60, "100%")
+    insertSort.svg.size((insertSort.items.length - 1) * 100 + 60, "100%")
 }
 
 function insertSortDrawFinishedArea(i) {
-    insertSort.svg.rect(100, (i)*40 +10)
-        .cx((insertSort.step-1)*100 + 30)
+    insertSort.svg.rect(100, (i) * 40 + 10)
+        .cx((insertSort.step - 1) * 100 + 30)
         .y(0)
         .fill("#546653")
         .back()
@@ -122,34 +122,34 @@ function insertSortDrawFinishedArea(i) {
 //================================================================================
 
 var mergeSort = {
-    svg: SVG().addTo("#merge-sort").size("100%","100%"),
+    svg: SVG().addTo("#merge-sort").size("100%", "100%"),
     step: 0,
     items: [],
     idPrefix: "merge"
 }
 
 function mergeSortAlgorithm(values) {
-    document.getElementById("merge-sort").style.height = ((itemCount+1)*40+30) + "px"
+    document.getElementById("merge-sort").style.height = ((itemCount + 1) * 40 + 30) + "px"
     mergeSort.svg.clear()
     mergeSort.step = 0
     mergeSort.items = []
     publish(copyArray(values), mergeSort)
-    for (let i = 2; i < values.length*2; i*=2) {
-        for (let j = 0; j <= Math.floor((values.length)/i); j++) {
-            let a = j*i
-            let b = j*i +i/2
-            for(let k = j*i; b < j*i+i && b < values.length && a < b; k++) {
+    for (let i = 2; i < values.length * 2; i *= 2) {
+        for (let j = 0; j <= Math.floor((values.length) / i); j++) {
+            let a = j * i
+            let b = j * i + i / 2
+            for (let k = j * i; b < j * i + i && b < values.length && a < b; k++) {
                 //console.log("i: " + i + " | j: " + j + " | k: " + k + " | a: " + a + " | b: " + b)
                 let originalA = a
                 let originalB = b
-                if(values[a].value<values[b].value) {
+                if (values[a].value < values[b].value) {
                     a++
                 } else {
                     shift(values, b, a)
                     a++
                     b++
                     //console.log(values)
-                    
+
                 }
                 let copy = copyArray(values)
                 copy[originalA].gotCompared = true
@@ -159,16 +159,16 @@ function mergeSortAlgorithm(values) {
             }
         }
     }
-    mergeSort.svg.size((mergeSort.items.length-1)*100+60, "100%")
+    mergeSort.svg.size((mergeSort.items.length - 1) * 100 + 60, "100%")
 }
 
 function mergeSortDrawComparisonArea(i, j) {
-    let startID = i*j
-    let endID = i*(j+1) -1
-    if(endID >= itemCount) endID = itemCount-1
-    let start = SVG(document.getElementById(mergeSort.idPrefix + (mergeSort.step-1) + "-" + startID))
-    let end = SVG(document.getElementById(mergeSort.idPrefix + (mergeSort.step-1) + "-" + endID))
-    mergeSort.svg.line(start.cx(), start.cy(), end.cx(), end.cy()).stroke({color: "#4d4d4d", width: 50, linecap: "round"}).back()
+    let startID = i * j
+    let endID = i * (j + 1) - 1
+    if (endID >= itemCount) endID = itemCount - 1
+    let start = SVG(document.getElementById(mergeSort.idPrefix + (mergeSort.step - 1) + "-" + startID))
+    let end = SVG(document.getElementById(mergeSort.idPrefix + (mergeSort.step - 1) + "-" + endID))
+    mergeSort.svg.line(start.cx(), start.cy(), end.cx(), end.cy()).stroke({ color: "#4d4d4d", width: 50, linecap: "round" }).back()
 }
 
 //================================================================================
@@ -176,14 +176,50 @@ function mergeSortDrawComparisonArea(i, j) {
 //================================================================================
 
 var quickSort = {
-    svg: SVG().addTo("#quick-sort").size("100%","100%"),
+    svg: SVG().addTo("#quick-sort").size("100%", "100%"),
     step: 0,
     items: [],
     idPrefix: "quick"
 }
 
-function quickSortAlgorithm(array) {
-    
+function quickSortAlgorithm(values) {
+    document.getElementById("quick-sort").style.height = ((itemCount + 1) * 40 + 30) + "px"
+    quickSort.svg.clear()
+    quickSort.step = 0
+    quickSort.items = []
+    let stack = []
+    stack.push({start: 0, end: values.length-1})
+    publish(copyArray(values), quickSort)
+    while(stack.length > 0) {
+        let element = stack.shift()
+        let offset = 0
+        let changed = false
+        for(let i = element.start; i < element.end-offset; i++) {
+            let shifted = false
+            if(values[i].value > values[element.end-offset].value) {
+                shift(values, i, element.end-offset)
+                shifted = true
+            } else {
+
+            }
+            let copy = copyArray(values)
+            copy[i].gotCompared = true
+            copy[element.end-offset].gotCompared = true
+            publish(copy, quickSort)
+            if(shifted) {
+                i--
+                offset++
+                changed = true
+            }
+        }
+        if(changed) {
+            let a = {start: element.start, end: element.end-offset-1}
+            let b = {start: element.end-offset+1, end: element.end}
+            if(a.end-a.start > 1) stack.push(a)
+            if(b.end-b.start > 1) stack.push(b)
+        }
+    }
+    quickSort.svg.size((quickSort.items.length - 1) * 100 + 60, "100%")
 }
 
 
@@ -203,8 +239,8 @@ function runRandom() {
         let index
         do {
             index = getRandom(itemCount)
-        } while(array[index] != null)
-        array[index] = {value: i}
+        } while (array[index] != null)
+        array[index] = { value: i }
     }
     runAlgorithms(array)
 }
@@ -212,7 +248,7 @@ function runRandom() {
 function runReverse() {
     let array = new Array(itemCount)
     for (let i = 0; i < itemCount; i++) {
-        array[i] = {value: itemCount-i-1}
+        array[i] = { value: itemCount - i - 1 }
     }
     runAlgorithms(array)
 }
@@ -221,6 +257,7 @@ function runAlgorithms(array) {
     bubbleSortAlgorithm(copyArray(array))
     insertSortAlgorithm(copyArray(array))
     mergeSortAlgorithm(copyArray(array))
+    quickSortAlgorithm(copyArray(array))
 }
 
 //================================================================================
@@ -236,13 +273,13 @@ function swap(array, a, b) {
 
 function shift(array, from, to) {
     let swap = array[from]
-    if(from > to) {
-        for(let i = from; i > to; i--) {
-            array[i] = array[i-1]
+    if (from > to) {
+        for (let i = from; i > to; i--) {
+            array[i] = array[i - 1]
         }
     } else {
-        for(let i = from; i < to; i++) {
-            array[i] = array[i+1]
+        for (let i = from; i < to; i++) {
+            array[i] = array[i + 1]
         }
     }
     array[to] = swap
@@ -252,7 +289,7 @@ function copyArray(array) {
     let copy = array.slice()
     for (let i = 0; i < copy.length; i++) {
         copy[i] = Object.assign({}, array[i])
-        
+
     }
     return copy
 }
@@ -262,13 +299,13 @@ function getRandom(max) {
 }
 
 function map(value, min, max, localMax) {
-    return ((max-min)/localMax) * value + min
+    return ((max - min) / localMax) * value + min
 }
 
 function getMax(array) {
     let m = 0
     array.forEach(element => {
-        if(element.value > m) m = element.value
+        if (element.value > m) m = element.value
     });
     return m
 }
@@ -276,8 +313,8 @@ function getMax(array) {
 function mapColor(value, localMax) {
     let rValue = map(value, 0, 255, localMax)
     let gValue = 70
-    let bValue = 255-rValue
-    return new SVG.Color({r: rValue, g: gValue, b: bValue})
+    let bValue = 255 - rValue
+    return new SVG.Color({ r: rValue, g: gValue, b: bValue })
     //return "rgb(" + rValue + ", " + gValue + ", " + bValue + ")"
 }
 
@@ -288,37 +325,37 @@ function publish(array, target) {
         target.svg
             .circle(
                 map(
-                    element.value, 
-                    MIN_CRICLE_SIZE, 
-                    MAX_CRICLE_SIZE, 
+                    element.value,
+                    MIN_CRICLE_SIZE,
+                    MAX_CRICLE_SIZE,
                     getMax(array)))
-            .cx(target.step*100 + 30)
-            .cy(i*40 + 30)
+            .cx(target.step * 100 + 30)
+            .cy(i * 40 + 30)
             .fill("#F5F6F7"/*mapColor(element.value, getMax(array))*/)
             .id(target.idPrefix + target.step + "-" + i)
-        if(target.step == 0) continue
-        if(element.gotCompared) {
-            SVG(document.getElementById(target.idPrefix + (target.step-1) + "-" + i)).stroke({ color: "#E8911A", width: 4})
+        if (target.step == 0) continue
+        if (element.gotCompared) {
+            SVG(document.getElementById(target.idPrefix + (target.step - 1) + "-" + i)).stroke({ color: "#E8911A", width: 4 })
         }
         //Draw connection line
-        let lastArray = target.items[target.step-1]
+        let lastArray = target.items[target.step - 1]
         for (let j = 0; j < lastArray.length; j++) {
             const lastElement = lastArray[j];
-            if(lastElement.value != element.value) continue
+            if (lastElement.value != element.value) continue
             //console.log("i: " + i + " | j: " + j + " | last: " + lastElement.value + " | current: " + element.value)
             target.svg
                 .line(
-                    (target.step-1)*100 + 30,   j*40 + 30, 
-                    target.step*100 + 30,       i*40 + 30)
-                .stroke({color: "#F5F6F7", width: 2}/*mapColor(element.value, getMax(array))*/)
-                .id(target.idPrefix + (target.step -1) + "-" + j + "---" + target.step + "-" + i)
+                    (target.step - 1) * 100 + 30, j * 40 + 30,
+                    target.step * 100 + 30, i * 40 + 30)
+                .stroke({ color: "#F5F6F7", width: 2 }/*mapColor(element.value, getMax(array))*/)
+                .id(target.idPrefix + (target.step - 1) + "-" + j + "---" + target.step + "-" + i)
         }
     }
     target.svg
-        .text(target.step+"")
-        .cx(target.step*100 + 30)
-        .cy(itemCount*40+30)
-        .font("family","Montserrat")
+        .text(target.step + "")
+        .cx(target.step * 100 + 30)
+        .cy(itemCount * 40 + 30)
+        .font("family", "Montserrat")
         .fill("#F5F6F7")
-    target.step ++
+    target.step++
 }
